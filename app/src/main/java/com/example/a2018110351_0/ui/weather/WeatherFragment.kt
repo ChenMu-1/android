a75076e2e5ef8b3b8aeb8be79da0764ef08b82c2
xiong <cm.chen890@gmail.com>
2020-12-27 23:04:07 +0800

    Third commit

--- app/src/main/java/com/example/a2018110351_0/ui/weather/WeatherFragment.kt
+++ app/src/main/java/com/example/a2018110351_0/ui/weather/WeatherFragment.kt
@@ -6,10 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
 import com.example.a2018110351_0.R
 import kotlinx.android.synthetic.main.activity_second.*
 
@@ -27,12 +30,14 @@ class WeatherFragment : Fragment() {
             ViewModelProviders.of(this).get(WeatherViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_weather, container, false)
         WeatherViewModel.cities.observe(requireActivity(), Observer {
+
             val cities = it
-            val adapter = ArrayAdapter<City>(this, android.R.layout.simple_list_item_1, cities)
-            listView.adapter = adapter
+
+//            val adapter = ArrayAdapter<City>(this, android.R.layout.simple_list_item_1, cities)
+//            listView.adapter = adapter
             listView.setOnItemClickListener { _, _, position, _ ->
                 val cityCode = cities[position].city_code
-                val intent = Intent(this, SecondActivity::class.java)
+                val intent = Intent(getActivity(), SecondActivity::class.java)
                 intent.putExtra("city_code", cityCode)
                 startActivity(intent)
             }
